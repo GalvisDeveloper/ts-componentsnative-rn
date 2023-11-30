e0ca9d27eb2a5b0d01bd714b01ffc36543f36688
GalvisDeveloper <galvisdeveloper@gmail.com>
2023-11-29 21:06:58 -0500

    Prompt android-iOS

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, Button, Alert } from 'react-native';
 import { styles } from '../theme/appTheme';
 
+import prompt from 'react-native-prompt-android';
+
 interface Props {
 	alertTitle?: string;
 	alertMessage?: string;
@@ -19,20 +21,35 @@ const AlertScreen = ({ alertTitle, alertMessage }: Props) => {
 					onPress: () => console.log('Cancel1 Pressed'),
 					style: 'destructive',
 				},
-                {
-					text: 'Cancel',
-					onPress: () => console.log('Cancel2 Pressed'),
-					style: 'destructive',
-				},
-                {
-					text: 'Cancel',
-					onPress: () => console.log('Cancel3 Pressed'),
-					style: 'destructive',
-				},
 			],
 			{
 				cancelable: true,
 				onDismiss: () => console.log('onDismiss'),
+			}
+		);
+	};
+
+	const showPrompt = () => {
+		// * IOS
+		// Alert.prompt(
+		// 	'Are you sure?',
+		// 	'This action cannot be undone',
+		// 	(text) => console.log('You entered: ' + text),
+		// 	'plain-text',
+		// 	'Hello World'
+		// );
+		prompt(
+			'Enter password',
+			'Enter your password to claim your $1.5B in lottery winnings',
+			[
+				{ text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+				{ text: 'OK', onPress: (password) => console.log('OK Pressed, password: ' + password) },
+			],
+			{
+				type: 'secure-text',
+				cancelable: false,
+				defaultValue: 'test',
+				placeholder: 'placeholder',
 			}
 		);
 	};
@@ -42,6 +59,7 @@ const AlertScreen = ({ alertTitle, alertMessage }: Props) => {
 			<Text>Alert</Text>
 
 			<Button title='Show Alert' onPress={showAlert} />
+			<Button title='Show Prompt' onPress={showPrompt} />
 		</View>
 	);
 };
